ff9a38df5ee7f5c39975155713dc19313b9eedc8
Zoltan Makranczi <zoltan.makranczi@anaplan.com>
2022-04-20 19:39:23 +0200

    CORS enabled globally

--- src/main/java/hu/vizicsaba/restapidemo/web/component/SecurityConfig.java
+++ src/main/java/hu/vizicsaba/restapidemo/web/component/SecurityConfig.java
@@ -46,6 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+            .cors()
+            .and()
             .csrf()
             .disable()
             .authorizeRequests()
